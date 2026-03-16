961dbc8c3d3deec9fa0e4c46a3e7c1b1b4a15829
Niirav213 <np5662948@gmail.com>
2026-03-16 08:57:07 +0530

    updated gitignore

--- asl-cv-module/encode_img.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-image_path = r"C:\Users\ASUS\Downloads\asl_alphabet_test\asl_alphabet_test\A_test.jpg"  # your image file name
-
-with open(image_path, "rb") as f:
-    encoded = base64.b64encode(f.read()).decode('utf-8')
-
-print(encoded)
